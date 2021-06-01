f6c62195942e87b57ff95bd822dac765d6e06af2
Alen Yohannan <erwinsmithscout1@gmail.com>
2021-06-01 21:23:57 +0530

    ..

--- src/lib/Server.ts
+++ src/lib/Server.ts
@@ -4,7 +4,6 @@ import WAClient from './WAClient'
 import { join } from 'path'
 
 export default class Server extends EventEmitter {
-
     app = express()
     WARouter = express.Router()
 
@@ -14,20 +13,20 @@ export default class Server extends EventEmitter {
         this.app.use('/wa', this.WARouter)
         this.WARouter.use(this.auth)
         this.WARouter.get('/qr', (req, res) => {
-            if (!this.client.QR) return void res.json({
-                message: (this.client.state === 'open') ? 'You\'re already authenticated' : 'QR is not generated yet'
-            })
+            if (!this.client.QR)
+                return void res.json({
+                    message: this.client.state === 'open' ? "You're already authenticated" : 'QR is not generated yet'
+                })
             res.contentType('image/png')
             return void res.send(this.client.QR)
         })
         this.app.listen(PORT, () => this.client.log(`Server Started on PORT: ${PORT}`))
     }
 
-    auth = (req: Request, res:  Response, next: NextFunction): void => {
+    auth = (req: Request, res: Response, next: NextFunction): void => {
         const { session } = req.query
-        if (!session) return void res.json({ message: `Sesion Query not provided`})
-        if (session !== this.client.config.session) return void res.json({ message: `Invalid Sesion ID`})
+        if (!session) return void res.json({ message: `Sesion Query not provided` })
+        if (session !== this.client.config.session) return void res.json({ message: `Invalid Sesion ID` })
         next()
     }
-    
 }
