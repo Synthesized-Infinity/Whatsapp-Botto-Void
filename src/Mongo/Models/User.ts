3d4ef81c9b6a9bbd93828838cc1ac4ef23998436
Alen Yohannan <erwinsmithscout1@gmail.com>
2021-06-02 13:11:16 +0530

    add mongoose schema

---
+++ src/Mongo/Models/User.ts
@@ -0,0 +1,22 @@
+
+import { Schema, model } from 'mongoose'
+import { IUserModel } from '../../typings'
+
+const UserSchema = new Schema({
+    jid: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    ban: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    warnings: {
+        type: Number,
+        required: true,
+        default: 0
+    }
+})
+export const user = model<IUserModel>('users', UserSchema)
