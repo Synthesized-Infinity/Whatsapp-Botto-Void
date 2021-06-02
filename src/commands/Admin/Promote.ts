25a271ea1ede8ccf4fa9e76ed5c221f0c39f7184
Alen Yohannan <erwinsmithscout1@gmail.com>
2021-06-02 11:18:56 +0530

    CQ

--- src/commands/Admin/Promote.ts
+++ src/commands/Admin/Promote.ts
@@ -9,7 +9,8 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        if (!M.groupMetadata?.admins?.includes(this.client.user.jid)) return void M.reply(`❌ Failed to ${this.config.command} as I'm not an admin`)
+        if (!M.groupMetadata?.admins?.includes(this.client.user.jid))
+            return void M.reply(`❌ Failed to ${this.config.command} as I'm not an admin`)
         if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
         if (!M.mentioned.length) return void M.reply(`Please tag the users you want to ${this.config.command}`)
         M.mentioned.forEach(async (user) => {
