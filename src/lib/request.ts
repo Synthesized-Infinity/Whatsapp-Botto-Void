1c55cbdc822d2c56d08438e766dab02bcca5750a
Alen Yohannan <alensaito1@users.noreply.github.com>
2021-06-14 16:54:30 +0530

    update `request.ts`

--- src/lib/request.ts
+++ src/lib/request.ts
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
-const request = async <T extends 'buffer' | 'json'>(url: string, type: T): Promise<T extends 'json' ? { [key: string]: string | boolean | number } : Buffer> => {
-    return (await axios.get(url, type === 'buffer' ?  {responseType: 'arraybuffer'} : undefined)).data
+const request = async <T extends 'buffer' | 'json'>(
+    url: string,
+    type: T
+): Promise<T extends 'json' ? { [key: string]: string | boolean | number } : Buffer> => {
+    return (await axios.get(url, type === 'buffer' ? { responseType: 'arraybuffer' } : undefined)).data
 }
 
 export default request
