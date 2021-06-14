42a3824b83edeb0d291d951d218013f37e0f1c8b
Alen Yohannan <erwinsmithscout1@gmail.com>
2021-06-14 14:10:39 +0530

    Fix `mongo buffering error`

--- src/void.ts
+++ src/void.ts
@@ -23,12 +23,7 @@ messageHandler.loadCommands()
 
 const db = mongoose.connection
 
-mongoose.connect(encodeURI(process.env.MONGO_URI), {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-})
-db.once('open', () => client.log(chalk.green('Connected to Database!')))
+
 
 new Server(Number(process.env.PORT) || 4000, client)
 
@@ -51,7 +46,18 @@ const start = async () => {
     await client.connect()
 }
 
-client.getAuthInfo(client.config.session).then((session) => {
-    if (session) client.loadAuthInfo(session)
-    start()
+mongoose.connect(encodeURI(process.env.MONGO_URI), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
 })
+
+db.once('open', () => {
+    client.log(chalk.green('Connected to Database!'))
+    client.getAuthInfo(client.config.session).then((session) => {
+        if (session) client.loadAuthInfo(session)
+        start()
+    })
+})
+
+
