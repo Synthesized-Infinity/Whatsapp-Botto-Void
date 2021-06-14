621aa667c7035c0ed80ed964506122cc5cd05e36
Alen Yohannan <alensaito1@users.noreply.github.com>
2021-06-14 15:47:37 +0530

    add `request` function as lib

---
+++ src/lib/request.ts
@@ -0,0 +1,7 @@
+import axios from 'axios'
+
+const request = async <T extends 'buffer' | 'json'>(url: string, type: T): Promise<T extends 'json' ? { [key: string]: string | boolean | number } : Buffer> => {
+    return (await axios.get(url, type === 'buffer' ?  {responseType: 'arraybuffer'} : undefined)).data
+}
+
+export default request
