adbac34a13fa15d1d0368ff99cd1f739b473cabb
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-06-17 13:13:56 +0530

    upadate `MessageHandler`

--- src/Handlers/MessageHandler.ts
+++ src/Handlers/MessageHandler.ts
@@ -27,19 +27,18 @@ export default class MessageHandler {
         )
         if (!command) return void M.reply('No Command Found! Try using one from the help list.')
         const user = await this.client.getUser(M.sender.jid)
-        if (user.ban) return void M.reply('You\'re Banned from using commands.')
+        if (user.ban) return void M.reply("You're Banned from using commands.")
         if (!command.config?.dm && M.chat === 'dm') return void M.reply('This command can only be used in groups')
         if (command.config?.adminOnly && !M.sender.isAdmin)
             return void M.reply(`Only admins are allowed to use this command`)
         try {
             await command.run(M, this.parseArgs(args))
             if (command.config.baseXp) {
-                await this.client.setXp(M.sender.jid, command.config.baseXp || 10, 50)  
+                await this.client.setXp(M.sender.jid, command.config.baseXp || 10, 50)
             }
         } catch (err) {
             return void this.client.log(err.message, true)
         }
-
     }
 
     loadCommands = (): void => {
