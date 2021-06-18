1e306db2f95c68186fbd9c39f3805dc51743e28f
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-06-18 14:15:52 +0530

    Update `Spotify.ts`

--- src/commands/Media/Spotify.ts
+++ src/commands/Media/Spotify.ts
@@ -4,27 +4,29 @@ import BaseCommand from '../../lib/BaseCommand'
 import request from '../../lib/request'
 import Spotify from '../../lib/Spotify'
 import WAClient from '../../lib/WAClient'
-import { IParsedArgs, ISimplifiedMessage } from '../../typings'
+import { ISimplifiedMessage } from '../../typings'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler)
     }
 
-    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
-        if (!joined.trim()) return void M.reply('Please provide a Valid YT URL')
-        const track = new Spotify(joined)
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        if (!M.urls.length) return void M.reply(`Please provide the Spotify Track URL that you want to download`)
+        const url = M.urls[0]
+        const track = new Spotify(url)
         const info = await track.getInfo()
-        if (info.error) return void M.reply(info.error)
+        if (info.error) return void M.reply(`Error Fetching: ${url}. Check if the url is valid and try again`)
+        const caption = `🎧 *Title:* ${info.name || ''}\n🎤 *Artists:* ${(info.artists || []).join(',')}\n💽 *Album:* ${
+            info.album_name
+        }\n📆 *Release Date:* ${info.release_date || ''}`
         M.reply(
             await request(info?.cover_url as string, 'buffer'),
             MessageType.image,
             undefined,
             undefined,
-            `🎧 *Title:* ${info.name || ''}\n🎤 *Artists:* ${(info.artists || []).join(',')}\n💽 *Album:* ${
-                info.album_name
-            }\n📆 *Release Date:* ${info.release_date || ''}`
-        )
+            caption
+        ).catch(() => M.reply(caption))
         M.reply(await track.getAudio(), MessageType.audio)
     }
 
