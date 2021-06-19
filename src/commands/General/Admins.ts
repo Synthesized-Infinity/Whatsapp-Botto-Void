36b1abfd9122b987f43729484484b3f673507f9b
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-06-19 22:03:47 +0530

    update `Admins` command

--- src/commands/General/Admins.ts
+++ src/commands/General/Admins.ts
@@ -14,9 +14,6 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        return void (await M.reply(`[ADMINS!]`, undefined, undefined, [
-            ...(M.groupMetadata?.admins || []),
-            M.sender.jid
-        ]))
+        return void (await M.reply(`ADMINS!\n[Tags Hidden]`, undefined, undefined, M.groupMetadata?.admins))
     }
 }
