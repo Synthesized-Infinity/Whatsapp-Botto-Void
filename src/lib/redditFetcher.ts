63f1c59a8c781ae0fe8cb1adefad7aca2f31162e
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-06-20 20:31:05 +0530

    add reddit fetcher

---
+++ src/lib/redditFetcher.ts
@@ -0,0 +1,25 @@
+import request from './request'
+
+interface IRedditResponse {
+    postLink: string
+    subreddit: string
+    title: string
+    url: string
+    nsfw: boolean
+    spoiler: boolean
+    author: string
+    ups: number
+    preview: string[]
+}
+
+export default async (subreddit: string): Promise<IRedditResponse | { error: string }> => {
+    try {
+        const response = await request<
+            'json', IRedditResponse
+        >(`https://meme-api.herokuapp.com/gimme/${subreddit}`, 'json')
+        if (!response.url) return { error: 'Invalid Subreddits'}
+        return response
+    } catch (err) {
+        return { error: 'Invalid Subreddits'}
+    }
+}
